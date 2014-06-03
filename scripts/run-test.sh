d13089a5ce3a07a4c2e29ace6b37f86f99ecfced
Irmak Can Ozsut <irmakcan@gmail.com>
2014-06-03 22:35:39 +0300

    Add travis and coveralls configs

---
+++ scripts/run-test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+xcodebuild clean test\
+  -sdk iphonesimulator \
+  -workspace ZSTToolkit.xcworkspace \
+  -scheme ZSTToolkitTests\
+  -configuration Debug \
+  OBJROOT=build \
+  ONLY_ACTIVE_ARCH=NO \
+  GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
+  GCC_GENERATE_TEST_COVERAGE_FILES=YES
