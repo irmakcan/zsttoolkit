9d10781e6e9635014829d6fdf6ae8aaefd8c1831
Irmak Ozsut <Irmak.Ozsut@monitise.com>
2016-04-22 01:05:43 +0300

    Add carthage support

--- ZSTToolkit/ZSTToolkit.h
+++ ZSTToolkit/ZSTToolkit.h
@@ -2,12 +2,16 @@
 //  ZSTToolkit.h
 //  ZSTToolkit
 //
-//  Created by Irmak Can Ozsut on 08/06/14.
-//  Copyright (c) 2014 Irmak Can Ozsut. All rights reserved.
+//  Created by Irmak Ozsut on 22/04/16.
+//  Copyright © 2016 Irmak Can Ozsut. All rights reserved.
 //
 
-#import "ZSTCategories.h"
+#import <UIKit/UIKit.h>
 
+FOUNDATION_EXPORT double ZSTToolkitVersionNumber;
+FOUNDATION_EXPORT const unsigned char ZSTToolkitVersionString[];
+
+#import "ZSTCategories.h"
 #import "ZSTAbsoluteTimer.h"
 #import "ZSTJSONObjectInitializationProtocol.h"
 #import "ZSTRGB.h"
