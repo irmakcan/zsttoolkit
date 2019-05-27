f2df012ee2444f6fb0bfaa56079c112c7a46d3a5
Irmak Can Ozsut <irmakcan@gmail.com>
2019-05-27 22:09:54 +0300

    Change ZSTRGB macros to be inline functions

--- ZSTToolkit/ZSTRGB.h
+++ ZSTToolkit/ZSTRGB.h
@@ -8,5 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
-#define ZSTRGB(r, g, b) [UIColor colorWithRed:((CGFloat)r)/255.f green:((CGFloat)g)/255.f blue:((CGFloat)b)/255.f alpha:1.f]
-#define ZSTRGBA(r, g, b, a) [UIColor colorWithRed:((CGFloat)r)/255.f green:((CGFloat)g)/255.f blue:((CGFloat)b)/255.f alpha:(a)]
+NS_ASSUME_NONNULL_BEGIN
+
+NS_INLINE UIColor * ZSTRGB(CGFloat r, CGFloat g, CGFloat b) {
+  return [UIColor colorWithRed:((CGFloat)r)/255.f green:((CGFloat)g)/255.f blue:((CGFloat)b)/255.f alpha:1.f];
+}
+
+NS_INLINE UIColor * ZSTRGBA(CGFloat r, CGFloat g, CGFloat b, CGFloat a) {
+  return [UIColor colorWithRed:((CGFloat)r)/255.f green:((CGFloat)g)/255.f blue:((CGFloat)b)/255.f alpha:(a)];
+}
+
+NS_ASSUME_NONNULL_END
